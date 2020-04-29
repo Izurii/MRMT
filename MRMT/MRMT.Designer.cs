491a82cd889c89bcd4b03398dedfabbb461074cc
Heitor Augusto Massarente <heitor.massarente@h2ocompany.com.br>
2020-04-29 19:37:05 -0300

    Initial commit.

---
+++ MRMT/MRMT.Designer.cs
@@ -0,0 +1,155 @@
+﻿namespace MRMT
+{
+    partial class MRMT
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MRMT));
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.dbPath = new System.Windows.Forms.OpenFileDialog();
+            this.button2 = new System.Windows.Forms.Button();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.button1.Location = new System.Drawing.Point(466, 13);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(128, 33);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Open DB";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Enabled = false;
+            this.textBox1.Location = new System.Drawing.Point(12, 19);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(446, 22);
+            this.textBox1.TabIndex = 1;
+            // 
+            // dbPath
+            // 
+            this.dbPath.FileName = "dbPath";
+            this.dbPath.Filter = "DB Files( (*.db)|*.db";
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(466, 94);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(128, 33);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Create CSV";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F);
+            this.checkBox1.Location = new System.Drawing.Point(12, 57);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(154, 28);
+            this.checkBox1.TabIndex = 3;
+            this.checkBox1.Text = "Author\'s name";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("MV Boli", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(7, 104);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(151, 26);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Izurii Hootoh";
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.button3.Location = new System.Drawing.Point(466, 52);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(128, 33);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "Extract DB";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.button4.Location = new System.Drawing.Point(301, 94);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(157, 33);
+            this.button4.TabIndex = 7;
+            this.button4.Text = "Export JSON";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // MRMT
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.GrayText;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.ClientSize = new System.Drawing.Size(606, 139);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "MRMT";
+            this.Text = "MRMT";
+            this.Load += new System.EventHandler(this.MRMT_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.OpenFileDialog dbPath;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ MRMT/MRMT.cs
@@ -0,0 +1,425 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Devart.Data.SQLite;
+using System.Diagnostics;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.Dynamic;
+using System.Text.RegularExpressions;
+
+namespace MRMT
+{
+    public partial class MRMT : Form
+    {
+
+        String csvPath, path;
+        Process p = new Process();
+        String currentDirectory = Directory.GetCurrentDirectory().ToString();
+        String curDirCor;
+        SQLiteDataReader reader, reader2;
+        SQLiteConnection dbConnection = new SQLiteConnection();
+        SQLiteCommand cmd, cmd2;
+        List<String> mangasN = new List<string>();
+        List<String> links = new List<string>();
+
+
+        public MRMT()
+        {
+            InitializeComponent();
+        }
+
+        private void MRMT_Load(object sender, EventArgs e)
+        {
+            checkBox1.Enabled = false; button2.Enabled = false; button4.Enabled = false;
+            cmd = dbConnection.CreateCommand();
+            cmd2 = dbConnection.CreateCommand();
+
+        }
+
+        /////////////////////////////////////////////////////////BUTTONS///////////////////////////////////////////////////////////////////////////////
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            dbPath.ShowDialog();
+            textBox1.Text = dbPath.FileName.ToString();
+            path = textBox1.Text.ToString();
+            checkBox1.Enabled = true; button2.Enabled = true; button4.Enabled = true;
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+            convertCSV();
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (File.Exists(@"mangarock.db") == true)
+            {
+                File.Delete(@"mangarock.db");
+            }
+            else
+            {
+                try
+                {
+                    getDB();
+                    Decompress();
+                    ExtractTar();
+                    cpdlFiles();
+                }
+                catch (Exception ex) { }
+                finally
+                {
+                    MessageBox.Show("DB Extracted.", "Yeee!", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+
+            dbConnection.ConnectionString = @"Data Source=" + path + ";FailIfMissing=False;";
+            dbConnection.Open();
+
+            try
+            {
+                createJson();
+            }
+            catch (Exception ex) { MessageBox.Show(ex.ToString()); }
+            finally
+            {
+                MessageBox.Show("It's done bro.", "Yeee!", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+
+        }
+
+        ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+       
+        ////////////////////////////////////////////////////////////////ExtractDB//////////////////////////////////////////////////////////////////////
+
+        void getDB()
+        {
+            try
+            {
+                p.StartInfo.FileName = addQuotes(currentDirectory + @"\adb.exe");
+                p.StartInfo.Arguments = "backup -noapk com.notabasement.mangarock.android.lotus";
+                p.Start();
+                p.WaitForExit();
+            }
+            catch (Exception ex) { MessageBox.Show(ex.ToString()); }
+        }
+
+        void Decompress()
+        {
+            try
+            {
+                p.StartInfo.FileName = "java";
+                p.StartInfo.Arguments = "-jar " + addQuotes(currentDirectory) + "\abe.jar unpack backup.ab manga.tar";
+                p.Start();
+                p.WaitForExit();
+            }
+            catch (Exception ex) { MessageBox.Show(ex.ToString()); }
+
+        }
+
+        void ExtractTar()
+        {
+            try
+            {
+                MessageBox.Show(addQuotes(currentDirectory + @"\mangatar"));
+                if (!Directory.Exists(addQuotes(currentDirectory + @"\mangatar")))
+                {
+                    Directory.CreateDirectory(addQuotes(currentDirectory + @"\mangatar"));
+                }
+
+                p.StartInfo.FileName = addQuotes(currentDirectory + @"\tar.exe");
+                p.StartInfo.Arguments = "xvf manga.tar -C mangatar";
+                p.Start();
+                p.WaitForExit();
+            }
+            catch (Exception ex) { MessageBox.Show(ex.ToString()); }
+        }
+
+        void cpdlFiles()
+        {
+            try
+            {
+                File.Copy(addQuotes(currentDirectory + @"\mangatar\apps\com.notabasement.mangarock.android.lotus\db\mangarock.db"), addQuotes(currentDirectory + "\\mangarock.db"));
+                if (Directory.Exists(addQuotes(currentDirectory + @"\mangatar")))
+                {
+                    Directory.Delete(addQuotes(currentDirectory + @"\mangatar"), true);
+                }
+                File.Delete(addQuotes(currentDirectory + @"\backup.ab"));
+                File.Delete(addQuotes(currentDirectory + @"\manga.tar"));
+            }
+            catch (Exception ex) { MessageBox.Show(ex.ToString()); }
+            
+        }
+
+        ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+        ///////////////////////////////////////////////////////////////////exportCSV///////////////////////////////////////////////////////////////////
+
+        void convertCSV()
+        {
+
+            dbConnection.ConnectionString = @"Data Source=" + path + ";FailIfMissing=False;";
+
+            try
+            {
+
+                dbConnection.Open();
+                cmd.CommandText = "SELECT manga_name, author, last_read FROM Favorites";
+                reader = cmd.ExecuteReader();
+
+                using (StreamWriter sw = File.CreateText(@"mangalist.csv"))
+                {
+                    try
+                    {
+
+                        if (checkBox1.Checked)
+                        {
+                            sw.WriteLine(String.Format("{0};{1}", "Manga name", "Author's name") + "\n");
+                            while (reader.Read())
+                            {
+                                byte[] bytes = Encoding.Default.GetBytes(String.Format("{0};{1}", reader.GetString("manga_name"), reader.GetString("author")));
+                                string mangainf = Encoding.UTF8.GetString(bytes);
+                                sw.WriteLine(mangainf);
+                            }
+                        }
+
+                        if (!checkBox1.Checked)
+                        {
+
+                            sw.WriteLine(String.Format("{0}", "Manga name") + "\n");
+                            while (reader.Read())
+                            {
+                                byte[] bytes = Encoding.Default.GetBytes(String.Format("{0}", reader.GetString("manga_name")));
+                                string mangainf = Encoding.UTF8.GetString(bytes);
+                                sw.WriteLine(mangainf);
+                            }
+
+                        }
+
+                    }
+                    catch (Exception ex) { MessageBox.Show(ex.ToString()); }
+                    finally
+                    {
+                        MessageBox.Show("It's done bro.", "Yeee!", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    }
+
+                }
+                dbConnection.Close();
+            }
+            catch (Exception ex) { MessageBox.Show(ex.ToString()); }
+
+        }
+
+        ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+        //////////////////////////////////////////////////////////////////exportJSON///////////////////////////////////////////////////////////////////
+
+        class Parent
+        {
+            public String version { get; set; }
+            public List<object> mangas { get; set; }
+        }
+
+        class Manga
+        {
+
+            public List<object> manga { get; set; }
+            public List<Chapter> chapters { get; set; }
+
+        }
+
+        class Chapter
+        {
+
+            public string u { get; set; }
+            public int r { get; set; }
+
+        }
+
+        void createJson()
+        {
+
+            var json = new Parent()
+            {
+                version = "2",
+                mangas = Mangaa(),
+            };
+
+            var sjson = JsonConvert.SerializeObject(json, Formatting.Indented);
+            File.WriteAllText("tachiyomi.json", sjson);
+            links.Clear();
+
+        }
+
+        List<object> Mangaa()
+        {
+            List<object> manga = new List<object>();
+            cmd.CommandText = "SELECT * FROM Favorites";
+            reader = cmd.ExecuteReader();
+            while (reader.Read())
+            {
+                if (reader.GetString("source_id") == "1") // MangaEden
+                { mangasN.Add(reader.GetString("manga_id")); }
+                else
+
+                if (reader.GetString("source_id") == "4") //MangaReader
+                { mangasN.Add(reader.GetString("manga_id")); }
+                else
+
+                if (reader.GetString("source_id") == "71") //MangaRock
+                { mangasN.Add(reader.GetString("manga_id")); }
+
+            }
+
+            for (int i = 0; i < mangasN.Count; i++)
+            {
+
+                cmd2.CommandText = "SELECT * FROM Favorites WHERE manga_id = '" + mangasN[i] + "'";
+                reader2 = cmd2.ExecuteReader();
+                while (reader2.Read())
+                {
+
+                    String nameEdenReader = reader2.GetString("manga_name").Replace(' ', '-').ToLower();
+
+                    if (reader2.GetString("source_id") == "1")
+                    {
+                        //MangaEden
+                        manga.Add(
+                        new Manga()
+                        {
+                            manga = new List<object> { "/en/en-manga/" + nameEdenReader, reader2.GetString("manga_name"), 6894303465364688269, 0, 0 },
+                            chapters = Chapters(reader2.GetString("manga_id"), reader2.GetString("source_id"), nameEdenReader)
+
+                        });
+                    }
+
+                    if (reader2.GetString("source_id") == "4")
+                    {
+                        //MangaReader
+                        manga.Add(
+                        new Manga()
+                        {
+                            manga = new List<object> { "/en/en-manga/" + nameEdenReader, reader2.GetString("manga_name"), 789561949979941461, 0, 0 },
+                            chapters = Chapters(reader2.GetString("manga_id"), reader2.GetString("source_id"), nameEdenReader)
+
+                        });
+
+                    }
+
+                    if (reader2.GetString("source_id") == "71")
+                    {
+                        //MangaRock
+                        manga.Add(
+                        new Manga()
+                        {
+                            manga = new List<object> { "/manga/" + reader2.GetString("manga_oid"), reader2.GetString("manga_name"), 1554176584893433663, 0, 0 },
+                            chapters = Chapters(reader2.GetString("manga_id"), reader2.GetString("source_id"), null)
+
+                        });
+
+                    }
+
+
+                }
+            }
+
+            return manga;
+        }
+
+        String correctedChapter(String chapter)
+        {
+
+            int index = chapter.IndexOf(':');
+            string correct;
+
+            if (index >= 0)
+            {
+                correct = Regex.Replace(chapter.Substring(0, index), "[^0-9]", "");
+                correct.Replace(" ", "");
+
+            }
+            else
+            { correct = chapter; }
+
+            return correct;
+
+        }
+
+        List<Chapter> Chapters(String mId, String sId, String correctedName)
+        {
+            links.Clear();
+            List<Chapter> chapters = new List<Chapter>();
+            chapters.Clear();
+            cmd.CommandText = "SELECT * FROM MangaChapter WHERE manga_id = '" + mId + "'";
+            reader = cmd.ExecuteReader();
+            while (reader.Read())
+            {
+
+                if (sId == "1") //MangaEden
+                { links.Add(correctedChapter(reader.GetString("title"))); }
+
+                if (sId == "4") //MangaReader
+                    links.Add(correctedChapter(reader.GetString("title")));
+
+                if (sId == "71") //MangaRock
+                    links.Add(reader.GetString("oid"));
+
+            }
+            for (int i = 0; i < links.Count; i++)
+            {
+                if (sId == "1") //MangaEden
+                {
+                    chapters.Add(new Chapter()
+                    {
+                        u = "/en/en-manga/" + correctedName + "/" + links[i] + "/1/",
+                        r = 1
+                    });
+                }
+                if (sId == "4") //MangaReader
+                {
+                    chapters.Add(new Chapter()
+                    {
+                        u = "/en/en-manga/" + correctedName + "/" + links[i] + "/1/",
+                        r = 1
+                    });
+                }
+                if (sId == "71") //MangaRock
+                {
+                    chapters.Add(new Chapter()
+                    {
+                        u = "/pagesv2?oid=" + links[i],
+                        r = 1
+                    });
+                }
+
+            }
+            return chapters;
+        }
+
+        ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+        public string addQuotes(string path)
+        {
+            return !string.IsNullOrWhiteSpace(path) ?
+                path.Contains(" ") && (!path.StartsWith("\"") && !path.EndsWith("\"")) ?
+                    "\"" + path + "\"" : path :
+                    string.Empty;
+        }
+
+    }
+
+}
